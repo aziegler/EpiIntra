52098b014c5477feb179986e8a79446a11eef06b
Xephi59 <xephirot59@gmail.com>
2015-01-13 19:59:58 +0100

    Begin getToken method

--- EpiIntraAPI/GetDatasFromIntra.cs
+++ EpiIntraAPI/GetDatasFromIntra.cs
@@ -1,22 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Linq;
+using Windows.Data.Json;
 
 namespace EpiIntraAPI
 {
 
     public class GetDataFromIntra
     {
-        public string url = "";
-        private string login;
-        private string password;
+        public string url = "https://epitech-api.herokuapp.com/";
+        protected string login;
+        protected string password;
 
         public GetDataFromIntra(string url, string login, string password)
         {
-            this.url = url;
+            if (url != null && url != "")
+                this.url = url;
             this.login = login;
             this.password = password;
+        }
+
+        public string getToken()
+        {
+            Uri uri = new Uri(url);
+            WebRequest request = WebRequest.Create(uri + "/login");
+            JsonObject jso = new JsonObject();
+            jso.Add("login", JsonValue.CreateStringValue(login));
+            jso.Add("password", JsonValue.CreateStringValue(password));
+            request.Credentials = CredentialCache.DefaultCredentials;
+            request.Method = "POST";
+            request.ContentType = "text/json";
         }
     }
 }
