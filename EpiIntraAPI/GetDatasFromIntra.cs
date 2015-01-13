963bf104641c2f2ae01d238a93bef254fa4377a0
Xephi59 <xephirot59@gmail.com>
2015-01-13 19:26:10 +0100

    Push Initial Classes

---
+++ EpiIntraAPI/GetDatasFromIntra.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EpiIntraAPI
+{
+
+    public class GetDataFromIntra
+    {
+        public string url = "";
+        private string login;
+        private string password;
+
+        public GetDataFromIntra(string url, string login, string password)
+        {
+            this.url = url;
+            this.login = login;
+            this.password = password;
+        }
+    }
+}
